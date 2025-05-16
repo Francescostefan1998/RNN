4d67ac416d56f181475ccf44ce56d3433f27c1b4
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-16 09:11:31 +0200

    encoding and decoding the character

--- character-levelLanguage.py
+++ character-levelLanguage.py
@@ -11,3 +11,13 @@ print('start index: ', start_indx)
 print('end index: ', end_indx)
 print('Total Length:', len(text))
 print('Unique Characters:', len(char_set))
+
+# We convert the character into integer and vice versa
+# Building the dictionary
+chars_sorted = sorted(char_set)
+char2int = {ch:i for i,ch in enumerate(chars_sorted)}
+char_array = np.array(chars_sorted)
+text_encoded = np.array([char2int[ch] for ch in text], dtype=np.int32)
+print('Text encoded shape:', text_encoded.shape)
+print(text[:15], '== Encoding ==>', text_encoded.shape)
+print(text_encoded[15:21], '== Reverse ==>', ''.join(char_array[text_encoded[15:21]]))
